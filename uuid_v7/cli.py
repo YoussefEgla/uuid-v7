5f6b0d1709c7603e83f041948673aa537d471036
YoussefEgla <YoussefEgla@users.noreply.github.com>
2024-02-14 20:12:08 +0000

    ✅ Ready to clone and code.

--- uuid_v7/cli.py
+++ uuid_v7/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for uuid_v7 project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m uuid_v7` and `$ uuid_v7 `.
 
     This is your program's entry point.
 

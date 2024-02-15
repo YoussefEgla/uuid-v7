9429f5f7d43c5f43b1b744120e94c9c140c10073
Youssef AbouEgla <youssefegla@gmail.com>
2024-02-15 12:22:19 +0200

    add sortable tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -14,6 +14,14 @@ def test_uuid7_correct_length():
     assert len(str(uuid7())) == 36
 
 
+def test_uuid7_returns_sortable_values():
+    uuid7_1 = uuid7()
+    for _ in range(100000):
+        uuid7_2 = uuid7()
+        assert uuid7_1 < uuid7_2
+        uuid7_1 = uuid7_2
+
+
 def test_uuid6_returns_uuid_instance():
     assert isinstance(uuid6(), UUID)
 
@@ -25,3 +33,11 @@ def test_uuid6_returns_unique_values():
 
 def test_uuid6_correct_length():
     assert len(str(uuid6())) == 36
+
+
+def test_uuid6_returns_sortable_values():
+    uuid6_1 = uuid6()
+    for _ in range(100000):
+        uuid6_2 = uuid6()
+        assert uuid6_1 < uuid6_2
+        uuid6_1 = uuid6_2

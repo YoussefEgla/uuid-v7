6f8c5a826d3e10d71d474c025fab320731097605
Youssef AbouEgla <youssefegla@gmail.com>
2024-02-15 12:03:08 +0200

    extend built-in UUID class for uuid6, uuid7

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Python setup.py for uuid_v7 package"""
+
 import io
 import os
 from setuptools import find_packages, setup
@@ -32,15 +33,14 @@ def read_requirements(path):
 setup(
     name="uuid_v7",
     version=read("uuid_v7", "VERSION"),
-    description="Awesome uuid_v7 created by YoussefEgla",
+    description="UUID v6 and v7 for Python 3.10+.",
     url="https://github.com/YoussefEgla/uuid-v7/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="YoussefEgla",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
-    entry_points={
-        "console_scripts": ["uuid_v7 = uuid_v7.__main__:main"]
-    },
+    entry_points={"console_scripts": ["uuid_v7 = uuid_v7.__main__:main"]},
     extras_require={"test": read_requirements("requirements-test.txt")},
+    python_requires=">=3.10",
 )
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,27 @@
-from uuid_v7.base import NAME
+from uuid_v7.base import uuid6, uuid7, UUID
 
 
-def test_base():
-    assert NAME == "uuid_v7"
+def test_uuid7_returns_uuid_instance():
+    assert isinstance(uuid7(), UUID)
+
+
+def test_uuid7_returns_unique_values():
+    for _ in range(1000):
+        assert uuid7() != uuid7()
+
+
+def test_uuid7_correct_length():
+    assert len(str(uuid7())) == 36
+
+
+def test_uuid6_returns_uuid_instance():
+    assert isinstance(uuid6(), UUID)
+
+
+def test_uuid6_returns_unique_values():
+    for _ in range(1000):
+        assert uuid6() != uuid6()
+
+
+def test_uuid6_correct_length():
+    assert len(str(uuid6())) == 36
